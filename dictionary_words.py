736754cdd8d7d6fd3936923d6422d01ed81f7967
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-01-04 23:40:27 +0000

    Added dictionary_words.py and sample_file.txt

---
+++ dictionary_words.py
@@ -0,0 +1,10 @@
+"""
+We will make several assumptions to reduce the complexity of this program:
+
+The program only accepts one argument: the number of words to be selected.
+All parameters except the number of words will be hard-coded.
+We will use the words file which is available on all Unix systems for our list of words.
+The sentences do not have to make grammatical sense.
+Word selection can be completely random and the word order does not matter.
+"""
+
