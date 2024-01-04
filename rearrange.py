c142ce3661a723a532217d3d49c49ab5fde41ed5
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-01-04 23:24:38 +0000

    Added rearrange fuction The fuction shuffles the words and join them into a string

--- rearrange.py
+++ rearrange.py
@@ -8,5 +8,26 @@ def custom_suffle(words):
         j = random.randint(0, i)
         #Swap the element s at indicice i and j.
         words[i], words[j] = words[j], words[i]
-    #retrun words
+    #return shuffled list
     return words  
+
+#Define a fuction to rearrange the workds
+#simple example (not used)
+# def rearrange(words):
+#     random.shuffle(words)
+#     return ' '.join(words)
+
+def rearrange(words):
+    # Shuffle the words and join them into a string.
+    return ' '.join(custom_suffle(words))
+
+
+# Check if the script is being run directly (as opposed to being imported).
+if __name__ == "__main__":
+    # Check if any command-line arguments were provided.
+    if len(sys.argv) > 1:
+        # Store all command-line arguments (excluding the script name) in a list.
+        words = sys.argv[1:]
+        print(rearrange(words))
+    else:
+        print("No words provided, Please provide words as command -line arguments. ")
