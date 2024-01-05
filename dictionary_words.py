8eba7c7a9020db7c7c94a35e149aa2a16437e0a6
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-01-05 21:44:54 +0000

    Added dictionary_words module

--- dictionary_words.py
+++ dictionary_words.py
@@ -10,16 +10,33 @@ Word selection can be completely random and the word order does not matter.
 
 
 
+# f = open("sample_file.txt", "r")
+# #lets print only a specified number of words
+# line_count = 0;
+# for x in f:
+#     print(x)
+#     line_count += 1
 
-f = open("sample_file.txt", "r")
-#lets print only a specified number of words
-line_count = 0;
-for x in f:
-    print(x)
-    line_count += 1
+#     if line_count == 5:
+#         break
 
-    if line_count == 5:
-        break
+# f.close()
 
-f.close()
 
+def read_file(file, n):
+    f = open(file, "r")
+    line_count = 0
+    for w in f:
+        print(w)
+        line_count += 1
+        
+        if line_count == n:
+            break
+        
+    f.close()
+
+if __name__ == '__main__':
+    file = '/usr/share/dict/words'
+    num_of_words = 5
+    read_file(file, num_of_words)
+    
